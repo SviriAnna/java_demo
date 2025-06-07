cda9f132b5e885c5b6100f2245270dbe1a89aec9
SviriAnna <ausviridova@gmail.com>
2025-06-07 07:38:37 +0400

    HW2: Added @Metric and @TimeLimitExceedLog aspects

---
+++ src/main/java/cache/CacheValue.java
@@ -0,0 +1,18 @@
+package cache;
+
+import lombok.Getter;
+
+public class CacheValue {
+    @Getter
+    private final Object value;
+    private final long expiryTime;
+
+    public CacheValue(Object value, long recordLifetime) {
+        this.value = value;
+        this.expiryTime = System.currentTimeMillis() + recordLifetime;
+    }
+
+    public boolean isExpired() {
+        return System.currentTimeMillis() > expiryTime;
+    }
+}
